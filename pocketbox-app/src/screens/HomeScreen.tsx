425e2f6e2636f390d45450af1840f130ce5a40e6
jeanbarbosa <programmer.jean@gmail.com>
2024-06-09 19:04:13 -0300

    chore: add nativewind and tailwind css

---
+++ pocketbox-app/src/screens/HomeScreen.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+import { View, Text } from "react-native"
+
+export function HomeScreen() {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-3xl text-blue-800">Home</Text>
+    </View>
+  )
+}
