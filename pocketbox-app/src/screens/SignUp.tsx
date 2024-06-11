006c3bf2acb8e2da2d521cabc2001a3c36d5f1cc
jeanbarbosa <programmer.jean@gmail.com>
2024-06-11 14:01:08 -0300

    refact: create user account

--- pocketbox-app/src/screens/SignUp.tsx
+++ pocketbox-app/src/screens/SignUp.tsx
@@ -3,6 +3,10 @@ import { SafeAreaView } from "react-native-safe-area-context"
 import * as Animatable from "react-native-animatable"
 import { useNavigation } from "@react-navigation/native"
 import { AuthNavigatorRoutesProps } from "../routes/auth.routes"
+import { useForm, Controller } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { z } from "zod"
+
 import {
   View,
   Text,
@@ -11,9 +15,61 @@ import {
   ScrollView,
   TouchableOpacity,
 } from "react-native"
+import { api } from "../services/api"
+import { Input } from "../components/Input"
+
+type FormDataProps = {
+  firstName: string
+  email: string
+  password: string
+}
+
+const signUpSchema = z.object({
+  firstName: z
+    .string({ required_error: "Nome obrigatório" })
+    .min(3, "mínimo 3 caracteres"),
+  email: z
+    .string({ required_error: "e-mail obrigatório" })
+    .email("e-mail inválido"),
+  password: z
+    .string({ required_error: "senha obrigatória" })
+    .min(6, "mínimo 6 caracteres"),
+})
 
 export function SignUp() {
   const navigation = useNavigation<AuthNavigatorRoutesProps>()
+
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormDataProps>({
+    resolver: zodResolver(signUpSchema),
+  })
+
+  async function handleSignUp({ firstName, email, password }: FormDataProps) {
+    try {
+      console.log(firstName, email, password)
+      const response = await api.post("/auth/register", {
+        firstName,
+        email,
+        password,
+      })
+
+      if (response.status !== 201) {
+        const { data } = response
+        throw new Error(data.message)
+      }
+
+      alert("Conta criada com sucesso!")
+      navigation.navigate("signIn")
+    } catch (error) {
+      if (Array.isArray(error)) {
+        alert(error.join("\n"))
+      }
+      alert("Erro ao criar conta. Tente novamente mais tarde.")
+    }
+  }
 
   return (
     <ScrollView
@@ -36,35 +92,51 @@ export function SignUp() {
             </Text>
           </View>
           <View className="mx-4 flex justify-between items-center gap-3">
-            <Animatable.View
-              animation="slideInUp"
-              className="flex-row flex-1 p-4 bg-white rounded-2xl"
-            >
-              <Feather name="user" size={28} color="gray" />
-              <TextInput
-                placeholder="Seu Nome"
-                className="flex-1 ml-2 text-gray-800"
+            <Animatable.View animation="slideInUp">
+              <Controller
+                control={control}
+                name="firstName"
+                render={({ field: { onChange } }) => (
+                  <Input hasError={!!errors.firstName}>
+                    <Feather name="user" size={28} color="gray" />
+                    <Input.Field placeholder="Nome" onChangeText={onChange} />
+                  </Input>
+                )}
               />
             </Animatable.View>
-            <Animatable.View
-              animation="slideInUp"
-              className="flex-row flex-1 p-4 bg-white rounded-2xl"
-            >
-              <Fontisto name="email" size={28} color="gray" />
-              <TextInput
-                placeholder="email"
-                className="flex-1 ml-2 text-gray-800"
+            <Animatable.View animation="slideInUp">
+              <Controller
+                control={control}
+                name="email"
+                render={({ field: { onChange } }) => (
+                  <Input hasError={!!errors.email}>
+                    <Fontisto name="email" size={28} color="gray" />
+                    <Input.Field
+                      placeholder="e-mail"
+                      keyboardType="email-address"
+                      autoCapitalize="none"
+                      onChangeText={onChange}
+                    />
+                  </Input>
+                )}
               />
             </Animatable.View>
-            <Animatable.View
-              animation="slideInUp"
-              className="flex-row flex-1 p-4 bg-white rounded-2xl"
-            >
-              <Fontisto name="unlocked" size={28} color="gray" />
-              <TextInput
-                secureTextEntry
-                placeholder="Senha"
-                className="flex-1 ml-2 text-gray-800"
+            <Animatable.View animation="slideInUp">
+              <Controller
+                control={control}
+                name="password"
+                render={({ field: { onChange } }) => (
+                  <Input hasError={!!errors.password}>
+                    <Fontisto name="unlocked" size={28} color="gray" />
+                    <Input.Field
+                      onSubmitEditing={handleSubmit(handleSignUp)}
+                      secureTextEntry
+                      returnKeyType="send"
+                      placeholder="Senha"
+                      onChangeText={onChange}
+                    />
+                  </Input>
+                )}
               />
             </Animatable.View>
             <View className="flex-row flex-1 mt-4">
@@ -73,7 +145,10 @@ export function SignUp() {
                 delay={100}
                 className="flex-1"
               >
-                <TouchableOpacity className="flex-row justify-center items-center gap-2 bg-gray-800 p-4 px-14 rounded-2xl">
+                <TouchableOpacity
+                  onPress={handleSubmit(handleSignUp)}
+                  className="flex-row justify-center items-center gap-2 bg-gray-800 p-4 px-14 rounded-2xl"
+                >
                   <Text className="text-white text-xl font-semibold">
                     Criar Conta
                   </Text>
